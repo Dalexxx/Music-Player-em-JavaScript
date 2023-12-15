fdb878b75662a19bc4c3b842cf1eaf52ba31348f
David Alex Souza Santos <davidalex989@gmail.com>
2023-12-15 09:52:17 -0300

    att

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
 
 //variáveis que estou criando
 const JorgeBenJor = {
@@ -82,6 +84,18 @@ function nextSong(){
     playSong();
 }
 
+function updateProgressBar(){
+    const barWidth = (song.currentTime/song.duration)*100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width) * song.duration;
+    song.currentTime = jumpToTime;
+}
+
 //execuções de função
 initializeSong()
 
@@ -89,3 +103,5 @@ initializeSong()
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', jumpTo);
