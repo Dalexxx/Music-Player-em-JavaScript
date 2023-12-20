c01c18207512e5786a9d0d22533aae75d7479693
David Alex Souza Santos <davidalex989@gmail.com>
2023-12-20 09:34:32 -0300

    add repeat

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const previous = document.getElementById('previous');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
 
 //variáveis que estou criando
 const JorgeBenJor = {
@@ -43,6 +44,7 @@ const twentyfour = {
 
 let isPlaying = false;
 let isShuffled = false;
+let repeatOn = false;
 const originalPlaylist = [JorgeBenJor, nao_posso, sevenk, toxico, twentyfour];
 let sortedPlaylist = [...originalPlaylist]
 let index = 0;
@@ -136,6 +138,26 @@ function shuffleButtonClicked(){
     }
 }
 
+function repeatButtonClicked(){
+    if (repeatOn === false){
+        repeatOn = true;
+        repeatButton.classList.add('button-active')
+    }
+    else{
+        repeatOn = false;
+        repeatButton.classList.remove('button-active')
+    }
+}
+
+function nextOrRepeat(){
+    if(repeatOn === false){
+        nextSong();
+    }
+    else{
+        playSong();
+    }
+}
+
 //execuções de função
 initializeSong();
 
@@ -144,5 +166,7 @@ play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
 song.addEventListener('timeupdate', updateProgressBar);
+song.addEventListener('ended', nextOrRepeat); 
 progressContainer.addEventListener('click', jumpTo);
-shuffleButton.addEventListener('click', shuffleButtonClicked)
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
